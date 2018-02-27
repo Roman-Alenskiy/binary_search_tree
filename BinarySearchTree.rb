a5146ebc6ac526afb17d5c3c1661d938e9ec5d9e
Roman Alenskiy <kuperman88@gmail.com>
2018-02-27 16:25:55 +0300

    Added: #breadth_first_search,#show_tree, Refactored: #node_to_s, inherit

--- BinarySearchTree.rb
+++ BinarySearchTree.rb
@@ -8,9 +8,42 @@ class Node
         @right_child = nil
     end
 
+    def node_to_s(node)
+        return puts "\nRoot: #{@root.value}" if node == @root
+
+        print "Node: #{node.value}".ljust(13)
+
+        unless node.left_child.nil?  
+            print "LC: #{node.left_child.value}".ljust(10)
+        else
+            print "LC: nil".ljust(10)
+        end
+        
+        unless node.right_child.nil?  
+            print "RC: #{node.right_child.value}".ljust(10)
+        else
+            print "RC: nil".ljust(10)
+        end 
+
+        print "Parent: #{node.parent.value}".ljust(15)
+
+        unless node.parent.left_child.nil?  
+            print "P's LC: #{node.parent.left_child.value}".ljust(15)
+        else 
+            print "P's LC: nil".ljust(15)
+        end
+        
+        unless node.parent.right_child.nil?  
+            print "P's RC: #{node.parent.right_child.value}".ljust(15)
+        else 
+            print "P's RC: nil".ljust(10)
+        end 
+        puts
+    end
+
 end
 
-class BinarySearchTree
+class BinarySearchTree < Node
     attr_accessor :data, :root
 
     def initialize(data = nil)
@@ -30,14 +63,13 @@ class BinarySearchTree
         if @root.nil?
             @root = node
             current_root = @root
-            return puts "Root: #{@root.value}"
+            return
         end
 
         if (node.value < current_root.value) && (current_root.left_child.nil?)
             node.parent = current_root
             current_root.left_child = node
-            
-            return node_to_s(node)
+            return
         elsif (node.value < current_root.value) && !(current_root.left_child.nil?)
             place_node(node, current_root.left_child)
             return
@@ -46,36 +78,49 @@ class BinarySearchTree
         if (node.value > current_root.value) && (current_root.right_child.nil?)
             node.parent = current_root
             current_root.right_child = node
-            return node_to_s(node)
+            return
         elsif (node.value > current_root.value) && !(current_root.right_child.nil?)
             place_node(node, current_root.right_child)
             return
         end
     end
 
-    # Private methods
-    private 
-    
-    def node_to_s(node)
-        string =  "Node: #{node.value}     Parent: #{node.parent.value}"
-        unless node.parent.left_child.nil?  
-            string += "     Parent's LC: #{node.parent.left_child.value}"
-        else 
-            string += "     Parent's LC: nil"
+    def breadth_first_search(value)
+        queue = [@root]
+        i = 0
+        until queue.empty? do # !!!!!#each iterator dont work correctly!!!!!!!
+            if queue[i].value == value
+                puts 
+                node_to_s(queue[i])
+                return queue[i]
+            else
+                queue << queue[i].left_child unless queue[i].left_child.nil?
+                queue << queue[i].right_child unless queue[i].right_child.nil?
+                queue.shift
+                i = 0
+            end
         end
-        
-        unless node.parent.right_child.nil?  
-            string += "     Parent's RC: #{node.parent.right_child.value}"
-        else 
-            string += "     Parent's RC: nil"
-        end 
-        puts string
+        puts "\nnil"
+        return nil
+    end
+
+    def show_tree
+        queue = [@root]
+        i = 0
+        until queue.empty? do
+            node_to_s(queue[i])
+            queue << queue[i].left_child unless queue[i].left_child.nil?
+            queue << queue[i].right_child unless queue[i].right_child.nil?
+            queue.shift
+            i = 0
+        end
     end
 
 end
 
 tree = BinarySearchTree.new
-tree.data = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+tree.data = [2, 1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 tree.build_tree
-            
+tree.show_tree
+tree.breadth_first_search(3)
 
--- README.md
+++ README.md
@@ -6,15 +6,13 @@ This is implementation of Binary search tree on Ruby
 
 ### Classes:
 
-+ BinarySearchTree class, which represents BST.
-+ Node class, containing a @value, @parent, @left_child, @right_child attrs.
++ Node class, which represents separate node.
++ BinarySearchTree class, which represents BST, consisting of separate nodes (inherited from Node class).
 
 
 ### Methods:
 
 + #build_tree takes an array of data (e.g. [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]) and turns it into a binary tree full of Node objects appropriately placed (note: #build_tree doesnt litteraly build BST, it just iterate through an array).
 + #place_node takes each element of array (from #build_tree) and directly "places" each node, step by step building BST
-
-### Auxiliary methods:
-
-+ #node_to_s used to form a string with information about particular node for #place_node
++ #show_tree used to form information about all nodes of single BST
++ #node_to_s used to form a string with information about particular node
